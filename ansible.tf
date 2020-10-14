63280f5b5b7527b4a49862acffcfd801f0bcb619
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 16:55:53 -0600

    [UPDATE] Implement a trigger in install_ansible based upon timestamp.

--- ansible.tf
+++ ansible.tf
@@ -2,6 +2,9 @@ resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
     command = "chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
   }
+  triggers {
+      build_number = "${timestamp()}"
+  }
   depends_on = [google_container_cluster.gke]
 }
 
