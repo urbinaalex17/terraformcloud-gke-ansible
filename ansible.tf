bf9108971d260d839c6b445962fb043def71fac9
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 16:12:31 -0600

    [FIX] Dependecy declare as list.

--- ansible.tf
+++ ansible.tf
@@ -2,5 +2,5 @@ resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
     command = "apt install --help ; yum install --help"
   }
-  depends_on = google_container_cluster.gke
+  depends_on = [google_container_cluster.gke]
 }
