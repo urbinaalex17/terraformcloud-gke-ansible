62405e55827d58a63d4110a40959340182fd1775
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 16:23:15 -0600

    [UPDATE] Add initial install-ansible script.

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
-    command = "apt install --help ; yum install --help"
+    command = "scripts/install-ansible.sh"
   }
   depends_on = [google_container_cluster.gke]
 }
