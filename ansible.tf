9d557b4f15dc0056f7bbe8aa2c03f33beab485a7
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 17:02:50 -0600

    [UPDATE] Fix install_ansible trigger

--- ansible.tf
+++ ansible.tf
@@ -2,7 +2,7 @@ resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
     command = "chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
   }
-  triggers {
+  triggers = {
       build_number = "${timestamp()}"
   }
   depends_on = [google_container_cluster.gke]
