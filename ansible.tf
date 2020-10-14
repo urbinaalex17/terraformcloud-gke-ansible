ad0ffe007cc43ff83f8b46dab0beaf6ee56230a3
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 16:36:48 -0600

    [UPDATE] Remove ls command.

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
-    command = "ls -l ; chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
+    command = "chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
   }
   depends_on = [google_container_cluster.gke]
 }
