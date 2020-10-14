105e336424b42c0dcaf5d872d0da579da7528722
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 16:25:52 -0600

    [DEGUG] Add debug commands.

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
-    command = "scripts/install-ansible.sh"
+    command = "ls -l ; ./scripts/install-ansible.sh"
   }
   depends_on = [google_container_cluster.gke]
 }
