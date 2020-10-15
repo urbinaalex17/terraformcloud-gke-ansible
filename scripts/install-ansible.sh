5f648cd9c086672ff3ee38b6d07f97e7432e6712
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 19:07:29 -0600

    [UPDATE] Python command

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install requirements
-python3 -m pip install --user ansible==2.9.14 openshift
+python -m pip install --user ansible==2.9.14 openshift
 export PATH=$PATH:$HOME/.local/bin
 ansible-playbook --version
