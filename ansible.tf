30aecabbe4603496008e6d4cd9895a8f0d0e5172
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-14 18:03:54 -0600

    [UPDATE] Add command to know linux version.

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "install_ansible" {
   provisioner "local-exec" {
-    command = "chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
+    command = "lsb_release -a ; chmod +x scripts/install-ansible.sh ; ./scripts/install-ansible.sh"
   }
   triggers = {
       build_number = "${timestamp()}"
